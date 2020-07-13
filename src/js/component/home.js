fc4e236fab6171799b6e7d73d70ac373cf3597ca
Chris <christianOlmos@protonmail.com>
2020-07-13 00:23:10 +0000

    basic list displays, doesnt have input button

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -3,22 +3,20 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+const initialTodos = [
+	{ title: "Make the Bed" },
+	{ title: "Read Homework" },
+	{ title: "Walk the dog" }
+];
+
 //create your first component
 export function Home() {
+	const [todos, saveTodos] = React.useState(initialTodos);
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			{todos.map(t => (
+				<li key={t.title}>{t.title}</li>
+			))}
 		</div>
 	);
 }
