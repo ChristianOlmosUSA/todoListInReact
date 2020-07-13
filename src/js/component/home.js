a8c4fbed3560c90515b88024e3af86e179f269b1
Chris <christianOlmos@protonmail.com>
2020-07-13 00:28:30 +0000

    button now adds extra item

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -12,11 +12,20 @@ const initialTodos = [
 //create your first component
 export function Home() {
 	const [todos, saveTodos] = React.useState(initialTodos);
+
+	// create adding item button's function:
+	function createTodo(todoName) {
+		saveTodos(todos.concat({ title: todoName }));
+	}
+
 	return (
 		<div className="text-center mt-5">
 			{todos.map(t => (
 				<li key={t.title}>{t.title}</li>
 			))}
+			<button onClick={() => createTodo("new item dawg")}>
+				add new todo
+			</button>
 		</div>
 	);
 }
